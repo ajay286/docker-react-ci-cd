2bc378ba97e33d9f072bc4b156d7d5380f678765
Ajay Kumar <ajay_286@yahoo.com>
2020-12-10 17:24:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello There, this is being controlled via CI/CD flow.
         </p>
         <a
           className="App-link"
